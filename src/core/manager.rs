e9d8babc5d17c79d3c4d851eb615f2af9968d9a2
mikeyQwn@ <mikeyQwn@gmail.com>
2024-07-10 20:26:38 +0300

    Add builder

--- src/core/manager.rs
+++ src/core/manager.rs
@@ -1,8 +1,15 @@
-use std::collections::HashMap;
+use std::{
+    borrow::BorrowMut,
+    collections::HashMap,
+    io::{Cursor, Read},
+};
 
 use thiserror::Error;
 
-use super::encryptor::{Encryprtor, EncryprtorError};
+use super::{
+    encryptor::{AESEncryptor, Encryprtor, EncryprtorError},
+    hasher::{Hasher, Sha256Hasher},
+};
 
 #[derive(Error, Debug, PartialEq, Eq)]
 pub enum PasswordManagerError {
@@ -18,6 +25,7 @@ impl From<EncryprtorError> for PasswordManagerError {
     }
 }
 
+#[derive(Debug)]
 pub struct PasswordManager<T>
 where
     T: Encryprtor,
@@ -49,6 +57,65 @@ where
     }
 }
 
+pub struct PasswordManagerOptions<E, B>
+where
+    E: Encryprtor,
+    B: Read,
+{
+    hashed_key: String,
+
+    encryptor: E,
+    body: Option<B>,
+}
+
+impl PasswordManagerOptions<AESEncryptor, Cursor<Vec<u8>>> {
+    pub fn new<T: AsRef<[u8]>>(key: T) -> Self {
+        Self::new_with_hasher(key, Sha256Hasher::new().borrow_mut())
+    }
+
+    pub fn new_with_hasher<T, U>(key: T, hasher: &mut U) -> Self
+    where
+        T: AsRef<[u8]>,
+        U: Hasher,
+    {
+        let hashed_key = hasher.hash(key);
+
+        Self {
+            hashed_key: hashed_key.clone(),
+
+            encryptor: AESEncryptor::new(hashed_key),
+            body: None,
+        }
+    }
+}
+
+impl<E, B> PasswordManagerOptions<E, B>
+where
+    E: Encryprtor,
+    B: Read,
+{
+    pub fn with_encryptor<T, F>(self, f: F) -> PasswordManagerOptions<T, B>
+    where
+        T: Encryprtor,
+        F: FnOnce(&str) -> T,
+    {
+        let encryptor = f(self.hashed_key.as_str());
+
+        PasswordManagerOptions::<T, B> {
+            hashed_key: self.hashed_key,
+            encryptor: encryptor,
+            body: self.body,
+        }
+    }
+
+    pub fn build(self) -> PasswordManager<E> {
+        PasswordManager {
+            kv: HashMap::new(),
+            encryptor: self.encryptor,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::core::encryptor::AESEncryptor;
@@ -69,5 +136,21 @@ mod tests {
             pm.get_password("bar"),
             Err(PasswordManagerError::NoPasswordFound)
         );
+    }
+
+    #[test]
+    fn test_builder() {
+        let key = "Hello world!".to_string();
+        let pm = PasswordManagerOptions::new(&key)
+            .with_encryptor(|key| AESEncryptor::new(key))
+            .build();
+
+        match pm {
+            PasswordManager {
+                kv: v,
+                encryptor: _,
+            } if v.len() == 0 => {}
+            _ => panic!("Password manager built incorrectly"),
+        }
     }
 }
